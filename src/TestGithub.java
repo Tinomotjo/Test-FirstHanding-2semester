f4477b31a13ccf62780e5bc7114ae82490a2d950
Tinomotjo <Tino0118@stud.kea.dk>
2018-01-30 13:53:01 +0100

    My first commit

---
+++ src/TestGithub.java
@@ -0,0 +1,13 @@
+public class TestGithub
+{
+
+
+    // Create an app that counts form 1 to 10 and print it in the console?
+    public static void main(String[] args)
+    {
+        for (int i = 1; i <=10 ; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
